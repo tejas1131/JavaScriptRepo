b20d4fbc997c63617d1f3d8bb494627c81220835
Tejas Chavan <tejakc2002@gmail.com>
2025-08-11 15:27:46 +0530

    First Commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,36 @@
+/*
+let js = 'amazing';
+// if (js === 'amazing') alert('JavaScript is FUN!');7
+console.log(40 + 5);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Tejas";
+let first = 'Jonas';
+let firstNamePerson;
+let first_name_person;
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilds = 'JM';
+let PI = 3.14;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'coder';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
+console.log(job1 + job2);
+*/
+
+true;
+// console.log(true);
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+console.log(typeof javaScriptIsFun);
+console.log(typeof true);
